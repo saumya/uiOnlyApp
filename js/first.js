9925dc8eaf1d195e145d0ec0fdfc2b4169911192
saumya <2saumyaray@gmail.com>
2017-08-20 14:50:59 +0530

    done: Form-Add Customer. TODO: Delete Customer

--- js/first.js
+++ js/first.js
@@ -1,11 +1,12 @@
 console.log('first.js');
 //START - Application Object
 var appFirstObj = {
+  isFirstTime:true,
   onGotAllCustomerData: function(dataObj){
     console.log('onGotAllCustomerData');
     var total = dataObj.length;
     dataObj.map(function(cValue,cIndex,cArray){
-      console.log(cValue,cIndex,cArray);
+      //console.log(cValue,cIndex,cArray);
       //console.log(cValue.name);
       var sHtml = "<div class='shortDetail'><div>"+cValue.id+":"+cValue.name+":"+cValue.phone+"</div><div>"+cValue.address+"</div></div>";
       $('#idCustomers').append(sHtml);
@@ -15,19 +16,25 @@ var appFirstObj = {
   },
   onAppReadyWithCustomerData: function(){
     console.group('onAppReadyWithCustomerData');
-    console.log('============== Adding EventHandlers : Start ==============');
-    $("#idBtnAddCustomer").on('click',function(event){
-      var uName = $("#idUName").val();
-      var uPhone = $("#idUPhone").val();
-      var uAddress = $("#idUAddress").val();
-      console.log( uName,uPhone,uAddress );
-      restCalls.addNewCustomer({
-        customerName:uName,
-        customerPhone:uPhone,
-        customerAddress:uAddress
+    
+    if(this.isFirstTime===true){
+      this.isFirstTime = false;
+      console.log('============== Adding EventHandlers : Start ==============');
+      $("#idBtnAddCustomer").on('click',function(event){
+        var uName = $("#idUName").val();
+        var uPhone = $("#idUPhone").val();
+        var uAddress = $("#idUAddress").val();
+        console.log( uName,uPhone,uAddress );
+        restCalls.addNewCustomer({
+          customerName:uName,
+          customerPhone:uPhone,
+          customerAddress:uAddress
+        });
       });
-    });
-    console.log('============== Adding EventHandlers : End ==============');
+      console.log('============== Adding EventHandlers : End ==============');
+    }
+    
+    
     console.groupEnd();
   },
   onNewCustomerAdded: function(){
