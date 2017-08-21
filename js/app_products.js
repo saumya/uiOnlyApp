4b8f9d1798aa91cfd31f1aa608817daf44a3256c
saumya <2saumyaray@gmail.com>
2017-08-21 16:52:34 +0530

    wip: products section

--- js/app_products.js
+++ js/app_products.js
@@ -1,4 +1,4 @@
-console.log('app.js');
+console.log('app_products.js');
 //jQuery Entry - jQuery.ready()
 $(function(){
   console.log('loaded');
@@ -9,7 +9,7 @@ $(function(){
     window.location.href = 'http://localhost:8888/slim/ui_app_1/home.html';
   });
   $('#id_menu_customers').on('click',function(){ window.location.href = 'http://localhost:8888/slim/ui_app_1/customers.html'; });
-  $('#id_menu_products').on('click',function(){ window.location.href = 'http://localhost:8888/slim/ui_app_1/products.html'; });
+  //$('#id_menu_products').on('click',function(){ window.location.href = 'http://localhost:8888/slim/ui_app_1/products.html'; });
   // Get the Customers data
-  restCalls.getAllCustomers();
+  //restCalls.getAllCustomers();
 });//END: jQuery.ready()
