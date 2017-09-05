e2306c2f9bfb63c546607913ca0deab20cd7fca3
saumya <2saumyaray@gmail.com>
2017-09-05 16:41:34 +0530

    WIP: Reports screen

--- js/app_reports_home.js
+++ js/app_reports_home.js
@@ -26,6 +26,10 @@ var appReportHome = {
   getVersion:function(){
     return {'version':'1.0.0'};
   },
+  logVersion: function(){
+    console.log('%c version-' + this.getVersion().version +' ','background: #F00; color: #FFF');
+  },
+  allData:{ soldData:{}, customersData:{}, productsData:{} },
   init: function(){
     console.log('appHome:init');
     var that = this;
@@ -33,19 +37,32 @@ var appReportHome = {
   },
   onGotAllSoldData: function(resultData){
     console.log('onGotAllSoldData:',resultData);
+    var that = this;
+    //that.logVersion();
+    //this.logVersion();
     var totalTimesSold = resultData.length;
-    $('#id_totalTimesSold').html('Total '+totalTimesSold+' times Sold.');
+    //console.log(this.allData);
+    this.allData.soldData = resultData;
+    //console.log(this.allData);
+    //
+    $('#id_totalTimesSold').html('Total '+totalTimesSold+' transactions');
     resultData.map(function(cValue,cIndex,cArray){
-      console.log(cValue);
+      //console.log(that.getVersion());
+      
+      //console.log(cValue);
       //var sHtml = "<div id="+cValue.id+" class='shortDetail'><div> Customer ID = "+cValue.customer_id+": Product ID = "+cValue.product_id+"</div><div> Quantity = "+cValue.quantity+' : on '+cValue.s_date+"</div><div id=sold_"+cValue.id+" class='btnDelete'>Delete</div></div>";
       // Removed the DELETE button
       //var sHtml = "<div id="+cValue.id+" class='shortDetail'><div> Customer ID = "+cValue.customer_id+": Product ID = "+cValue.product_id+"</div><div> Quantity = "+cValue.quantity+' : on '+cValue.s_date+"</div></div>";
       var sHtml = "<div id="+cValue.id+" class='shortDetail'> <div>"+cValue.s_date+' : Nos.'+cValue.quantity+"</div> </div>";
       $('#id_all_sold').append(sHtml);
     });
+    //
+    restCalls.getAllCustomers(that);
   },
   onGotAllCustomerData: function(resultObj){
     console.log('onGotAllCustomerData:',resultObj);
+    this.allData.customersData = resultObj;
+    /*
     var total = resultObj.length;
     $('#idTotal_Cust').html(total);
     var resultObjProducts = restCalls.getAllProducts();
@@ -58,9 +75,49 @@ var appReportHome = {
       var totalCompanies = resultdata.length;
       $('#idTotal_Companies').html(totalCompanies);
     });
+    */
+    var that = this;
+    var resultProducts = restCalls.getAllProducts(that);
+    resultProducts.done(function(data){
+      that.allData.productsData = data;
+      that.renderWithAllData();
+    });
   },
   onAppReadyWithCustomerData: function(){
     console.log('%c Just a callback. Doing Nothing. ','background: #222; color: #bada55');
+  },
+  renderWithAllData: function(){
+    console.log('renderWithAllData');
+    console.log(this.allData);
+    $('#id_all_sold').html('');
+    var that = this;
+    this.allData.soldData.map(function(cValue,cIndex,cArray){
+      //console.log(cValue);
+      var sHtml = '';
+      //var sHtml = "<div id="+cValue.id+" class='shortDetail'> <div>"+cValue.s_date+' : Nos.'+cValue.quantity+"</div> </div>";
+      var i = 0;
+      var j = 0;
+      sHtml += "<div id="+cValue.id+" class='shortDetail'> <div> <span class='sDetails'>"+cValue.s_date+"</span><span class='sDetails'>"+cValue.quantity+"</span>";
+      //
+      for(i;i<that.allData.productsData.length;i++){
+        //console.log(cValue.product_id,that.allData.productsData[i]);
+        if(cValue.product_id === that.allData.productsData[i].id){
+          sHtml += "<span class='sDetails'>"+that.allData.productsData[i].name+"</span>";
+          break;
+        }
+      }
+      for(j;j<that.allData.customersData.length;j++){
+        if(cValue.customer_id === that.allData.customersData[j].id){
+          sHtml += "<span class='sDetails'>"+that.allData.customersData[j].name+"</span>";
+          break;
+        }
+      }
+      sHtml+='</div></div>';
+
+      $('#id_all_sold').append(sHtml);
+      //
+      $('#id_totalCustomers').html(that.allData.customersData.length+' Customers / '+that.allData.productsData.length+' Products');
+    });
   },
   end:function(){
     console.log('AppHome : End');
